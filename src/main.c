679f57ee9ed161a3828eca70ebc035f0a1059dd9
Natalia Lewandowska <natka@DESKTOP-EQFOD35.localdomain>
2022-12-15 00:43:46 +0100

    Dodano obsługę błędów

--- src/main.c
+++ src/main.c
@@ -7,24 +7,38 @@
 
 int main(int argc, char ** argv) {
 	int res;
-	Matrix * A = readFromFile(argv[1]);
-	Matrix * b = readFromFile(argv[2]);
-	Matrix * x;
+	Matrix *A = readFromFile(argv[1]);
+	Matrix *b = readFromFile(argv[2]);
+	Matrix *x;
 
 	if (A == NULL) return -1;
 	if (b == NULL) return -2;
+
 	printToScreen(A);
 	printToScreen(b);
 
 	res = eliminate(A,b);
 	x = createMatrix(b->r, 1);
+
 	if (x != NULL) {
 		res = backsubst(x,A,b);
+
+		if (res == 1)
+			fprintf(stderr, "Błąd! Po drodze probowano dzielic przez 0.\n");
+		else if (res == 2)
+			fprintf(stderr, "Błąd! Nieprawidlowe rozmiary macierzy.\n");
+
+		if (res == 1 || res == 2) {
+			freeMatrix(A);
+			freeMatrix(b);
+
+			return 1;
+		}
 
 		printToScreen(x);
 	  freeMatrix(x);
 	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+					fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
 
 	freeMatrix(A);
